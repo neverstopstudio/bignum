78b86aae8c42a0520c6137b3259e2c7f78bf6832
mbenz12 <mbenz10120112@gmail.com>
2024-03-29 16:36:05 +0200

    Initial Commit

---
+++ Bignum Watch App/BignumApp.swift
@@ -0,0 +1,17 @@
+//
+//  BignumApp.swift
+//  Bignum Watch App
+//
+//  Created by Vladyslav on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Bignum_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bignum Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bignum Watch App
+//
+//  Created by Vladyslav on 29.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
